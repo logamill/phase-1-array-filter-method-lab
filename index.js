40cdd57903870534ba53500edcdcf8cd44853864
+ <+>
2022-04-06 15:13:02 -0400

    commit tree 1d4677fd2df0f35a8d67dadf1659bd541ea5c7bb

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function findMatching(drivers, string) {
+    const whatever = drivers.filter(element => element.toUpperCase() === string.toUpperCase())
+    return whatever;
+};
+
+
+function fuzzyMatch(drivers, string) {
+    const whatever = drivers.filter(element => element[0] === string[0])
+    console.log(whatever);
+    return whatever;
+}
+
+function matchName(drivers, string) {
+    const whatever = drivers.filter(element => element.name === string)
+    return whatever;
+}
